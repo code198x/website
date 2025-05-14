6dde7f377d25ae2ee7f47757b3a529279368de5c
Steve Hill <steve@stevehill.xyz>
2025-05-14 21:56:30 +0100

    Set up Tailwind CSS and fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code198x.stevehill.xyz',
   output: 'static',
   integrations: [mdx()],
+
   markdown: {
     syntaxHighlight: 'prism',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
