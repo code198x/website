cf7a2a39d30b67074b248c115bcd151289d0805a
Steve Hill <steve@stevehill.xyz>
2025-06-28 07:54:11 +0100

    Updates

--- lesson1-example1-acme.s
+++
@@ -1,7 +0,0 @@
-; Neon Nexus - Lesson 1
-; Our first C64 program
-
-*= $c000               ; Tell C64 to load at $c000 (49152)
-
-start:
-        rts             ; Return to BASIC
--- lesson1-example2-acme.s
+++
@@ -1,9 +0,0 @@
-; Neon Nexus - Lesson 1
-; Border color change test
-
-*= $c000               ; Load at $c000 (49152)
-
-start:
-        lda #$02        ; Load red color
-        sta $d020       ; Store to border color register
-        rts
