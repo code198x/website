7f9c152323602cf157bd7f2a45b580361784df36
Steve Hill <steve@stevehill.xyz>
2026-09-19 08:59:25 +0100

    Move Dice Roller into Maths for Games

---
+++ src/lib/dice-roller-redirects.mjs
@@ -0,0 +1,17 @@
+// Dice Roller now teaches probability within Maths for Games.
+export const diceRollerRedirects = {
+  "/systems/sinclair-zx-spectrum/basic/dice-roller": "/craft/maths-for-games/",
+  "/systems/sinclair-zx-spectrum/basic/dice-roller/unit-01": "/craft/maths-for-games/unit-01/",
+  "/systems/sinclair-zx-spectrum/basic/dice-roller/unit-02": "/craft/maths-for-games/unit-02/",
+  "/systems/sinclair-zx-spectrum/basic/dice-roller/unit-03": "/craft/maths-for-games/unit-03/",
+  "/systems/sinclair-zx-spectrum/basic/dice-roller/unit-04": "/craft/maths-for-games/unit-04/",
+  "/systems/sinclair-zx-spectrum/basic/dice-roller/unit-05": "/craft/maths-for-games/unit-05/",
+  "/sinclair-zx-spectrum/basic/dice-roller": "/craft/maths-for-games/",
+  "/sinclair-zx-spectrum/basic/dice-roller/unit-01": "/craft/maths-for-games/unit-01/",
+  "/sinclair-zx-spectrum/basic/dice-roller/unit-02": "/craft/maths-for-games/unit-02/",
+  "/sinclair-zx-spectrum/basic/dice-roller/unit-03": "/craft/maths-for-games/unit-03/",
+  "/sinclair-zx-spectrum/basic/dice-roller/unit-04": "/craft/maths-for-games/unit-04/",
+  "/sinclair-zx-spectrum/basic/dice-roller/unit-05": "/craft/maths-for-games/unit-05/",
+  "/systems/sinclair-zx-spectrum/basic/game-05-dice-roller": "/craft/maths-for-games/",
+  "/sinclair-zx-spectrum/basic/game-05-dice-roller": "/craft/maths-for-games/"
+};
